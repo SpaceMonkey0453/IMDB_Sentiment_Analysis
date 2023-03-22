70878d2d816f09b3ab963254999a92c199af57bb
SpaceMonkey0453 <80290938+SpaceMonkey0453@users.noreply.github.com>
2023-03-22 02:57:19 -0400

    Code Finished

--- preprocessing.py
+++ preprocessing.py
@@ -7,55 +7,63 @@ from nltk.stem.wordnet import WordNetLemmatizer as wnl
 from nltk.stem import SnowballStemmer
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import pickle
 
 max_words = 20000
 maxlen = 500
 stop_words = set(stopwords.words('english'))
 # Get a list of valid English words
 word_list = words.words()
+# Define a function to replace all non-ASCII characters with a space
+def replace_non_ascii_regex(str_a):
+     # Remove non-ASCII characters
+    str_a = re.sub('[^\x00-\x7F]+', ' ', str_a)
+    
+    # Remove non-alphanumeric and non-whitespace characters
+    str_a = re.sub('[^0-9a-zA-Z\s]+', ' ', str_a)
+    return str_a
 
-def get_wordnet_pos(treebank_tag):
-    if treebank_tag.startswith('J'):
-        return 'a'
-    elif treebank_tag.startswith('V'):
-        return 'v'
-    elif treebank_tag.startswith('N'):
-        return 'n'
-    elif treebank_tag.startswith('R'):
-        return 'r'
-    else:
-        return None
-
-def replace_non_ascii(str_a):
-    return str_a.translate(str.maketrans('', '', ''.join([chr(i) for i in range(128, 256)])))
-
+# Define a function to clean a string by removing unnecessary characters
 def clean_str(string):
     return string.replace("\\", "").replace("'", "").replace('"', '').strip().lower()
 
-# Define a function to lemmatize a sentence using WordNetLemmatizer
+# Define a function to lemmatize a sentence using SnowballStemmer
 def lemmatize_sentence(sentence, word_list, stop_words):
-    # Create a WordNetLemmatizer object
+    # Create a SnowballStemmer object
     lemmatizer = SnowballStemmer(language='english')
     
-    # Tokenize the sentence and get the part of speech tag for each token
-    tagged = nltk.word_tokenize(sentence.lower())
+    # Tokenize the sentence into words
+    words = nltk.word_tokenize(sentence.lower())
     
-    # Lemmatize each valid token in the sentence and remove stop words and non-valid words
+    # Lemmatize each valid word in the sentence and remove stop words and non-valid words
     lemmatized_sentence = [
-        lemmatizer.stem(token)
-        for token in tagged
-        if token.lower() in word_list and token.lower() not in stop_words
+        lemmatizer.stem(word)
+        for word in words
+        if word.lower() in word_list and word.lower() not in stop_words
     ]
     
-    # Join the lemmatized tokens back into a sentence
+    # Join the lemmatized words back into a sentence
     return ' '.join(lemmatized_sentence)
 
+# Define a function to apply lemmatization to a Pandas DataFrame
 def apply_lemmatization(df):
     # Apply the lemmatize_sentence function to the 'review' column of the DataFrame
     return df['review'].apply(lambda x: lemmatize_sentence(x, word_list, stop_words))
 
-def preprocess(df, tokenizer_path='tokenizer.pickle'):
-    df['review'] = df['review'].apply(replace_non_ascii)
+def preprocess(df, tokenizer_path='tokenizer.pickle', max_len=500):
+    # Load the saved tokenizer
+    with open(tokenizer_path, 'rb') as handle:
+        tokenizer = pickle.load(handle)
+    
+    # Preprocess the text
+    df['review'] = df['review'].apply(replace_non_ascii_regex)
     df['review'] = df['review'].apply(clean_str)
     lemmatized_reviews = apply_lemmatization(df)
-    return lemmatized_reviews
+    
+    # Convert text to sequences of tokens
+    sequences = tokenizer.texts_to_sequences(lemmatized_reviews)
+    
+    # Pad the sequences to a fixed length
+    padded_sequences = pad_sequences(sequences, maxlen=max_len)
+    
+    return padded_sequences
